83d9c228ea370896a50dbf90bdb7cd73ccc02beb
Joseph Thompson <joseph@Josephs-MacBook-Pro.local>
2019-06-05 16:47:49 -0400

    finished main nav html

--- src/components/MainNav/MainNav.component.js
+++ src/components/MainNav/MainNav.component.js
@@ -24,44 +24,46 @@ export default class MainNav extends Component {
   toggle = () => this.setState({ isOpen: !this.state.isOpen });
   render() {
     return (
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Company Logo</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="#home">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#advantages">Advantages</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#features">Features</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#blog">Blog</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#testimonials">Testimonials</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#partners">Partners</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#prices">Prices</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#contact">Contact</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-        <InputGroup>
-          <InputGroupAddon addonType="prepend">
-            <FaSearch color="gray" size="1.6em" />
-          </InputGroupAddon>
-          <Input placeholder="username" />
-        </InputGroup>
-      </Navbar>
+      <section>
+        <Navbar color="light" light expand="md" id="nav">
+          <NavbarBrand href="#nav">Company Logo</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#home">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#advantages">Advantages</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#features">Features</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#blog">Blog</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#testimonials">Testimonials</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#partners">Partners</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#prices">Prices</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#contact">Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">
+              <FaSearch color="gray" size="1.6em" />
+            </InputGroupAddon>
+            <Input placeholder="search" />
+          </InputGroup>
+        </Navbar>
+      </section>
     );
   }
 }
